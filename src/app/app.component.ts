774b01f22c5d4c3e793c30746244817d47fd1918
codewarrior <33649805+tguructa@users.noreply.github.com>
2020-06-17 13:45:10 +0530

    added RECORD_AUDIO permission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class MyApp {
       androidPermissions.requestPermissions(
         [
           androidPermissions.PERMISSION.CAMERA, 
-          androidPermissions.PERMISSION.CALL_PHONE, 
+          androidPermissions.PERMISSION.RECORD_AUDIO, 
           androidPermissions.PERMISSION.GET_ACCOUNTS, 
           androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE, 
           androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE
